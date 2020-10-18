f4a8689e66b142bf38513044038e5c8a783a79f3
Thieu Mao <thieumao@gmail.com>
2020-10-19 03:11:44 +0700

    Spawn Obstacle

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject obstacle;
+
+    public float spawnTime;
+
+    float m_spawnTime;
+
+    int m_score;
+
+    bool m_isGameover;
+
+    void Start() {
+        m_spawnTime = 0;
+    }
+
+    void Update() {
+        m_spawnTime -= Time.deltaTime;
+
+        if (m_spawnTime <= 0) {
+            SpawnObstacle();
+			m_spawnTime = spawnTime;
+		}
+    }
+
+    public void SpawnObstacle() {
+        float ranYPos = Random.Range(-2.75f, -1f);
+
+        Vector2 spawnPos = new Vector2(11, ranYPos);
+
+        if (obstacle) {
+            Instantiate(obstacle, spawnPos, Quaternion.identity);
+        }
+    }
+
+    public void SetScore(int value) {
+        m_score = value;
+    }
+
+    public int GetScore() {
+        return m_score;
+    }
+
+    public void ScoreIncrement() {
+        m_score++;
+    }
+
+    public bool IsGameover() {
+        return m_isGameover;
+    }
+
+    public void SetGameoverState(bool state) {
+        m_isGameover = state;
+    }
+}
