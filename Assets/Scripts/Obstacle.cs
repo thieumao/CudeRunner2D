3ecb3fcc269805320cc73e3bcaed1427accd31ae
Thieu Mao <thieumao@gmail.com>
2020-10-19 02:32:01 +0700

    Moving

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+    public float moveSpeed;
+
+    void Start() {
+        
+    }
+
+    void Update() {
+        transform.position = transform.position + Vector3.left * moveSpeed * Time.deltaTime;
+    }
+}
