af87feda2bcf2c2bf1988de0c7c78d2f4dae95e8
Thieu Mao <thieumao@gmail.com>
2020-10-19 01:51:32 +0700

    Jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float jumpForce;
+
+    Rigidbody2D m_rb;
+
+    bool m_isGround;
+
+    void Start() {
+        m_rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        bool isJumpKeyPressed = Input.GetKeyDown(KeyCode.Space);
+        if (isJumpKeyPressed && m_isGround) {
+            m_rb.AddForce(Vector2.up * jumpForce);
+            m_isGround = false;
+        }
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            m_isGround = true;
+        }
+	}
+}
